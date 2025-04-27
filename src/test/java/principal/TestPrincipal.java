cff68de64f7295ff9b6aaeca5032ffe014154629
GarraOne <garrra.one@gmail.com>
2025-04-27 18:45:08 -0300

    ta ae

--- src/test/java/principal/TestPrincipal.java
+++ src/test/java/principal/TestPrincipal.java
@@ -6,13 +6,5 @@ import javax.swing.SwingUtilities;
 
 public class TestPrincipal {
     
-    @Test
-    public void testPrincipalCriaMenu() throws Exception {
-        SwingUtilities.invokeAndWait(() -> {
-            Principal.main(null); // Executa o método main
-            assertNotNull(Principal.menu, "O menu principal não foi inicializado.");
-            assertTrue(Principal.menu.isVisible(), "O menu principal não está visível.");
-            Principal.menu.dispose(); // Fecha a janela depois do teste
-        });
-    }
+    
 }
