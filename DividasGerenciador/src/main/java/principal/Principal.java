0e723b4a5138efe02cb766134fa8aee7ae2480d2
GarraOne <garrra.one@gmail.com>
2025-04-26 21:45:20 -0300

    Criação do Gerenciador de Dividas, contendo o principal e a introdução do menu

---
+++ DividasGerenciador/src/main/java/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+import visual.FrmMenuPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        //Abre o Menu Principal
+        FrmMenuPrincipal menu = new FrmMenuPrincipal();
+        menu.setVisible(true);
+    }
+}
